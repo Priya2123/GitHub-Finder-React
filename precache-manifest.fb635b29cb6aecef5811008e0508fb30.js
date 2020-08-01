078b2722e0cbf9d2de05ce78c43baec67acccbae
Priya Aggarwal <aggarwalpriya1121@gmail.com>
2020-08-01 19:16:02 +0530

    Updates

---
+++ precache-manifest.fb635b29cb6aecef5811008e0508fb30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6efbee23c6f3842fb5e585555189909",
+    "url": "/GitHub-Finder-React/index.html"
+  },
+  {
+    "revision": "65b537806297a2e63139",
+    "url": "/GitHub-Finder-React/static/css/main.c1ef5e3b.chunk.css"
+  },
+  {
+    "revision": "27df273aa531efc39cfe",
+    "url": "/GitHub-Finder-React/static/js/2.5b6c137e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GitHub-Finder-React/static/js/2.5b6c137e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65b537806297a2e63139",
+    "url": "/GitHub-Finder-React/static/js/main.ef18dc83.chunk.js"
+  },
+  {
+    "revision": "e331fecfbc5e3259b51a",
+    "url": "/GitHub-Finder-React/static/js/runtime-main.622bdeea.js"
+  }
+]);
