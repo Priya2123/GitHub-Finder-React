cc19b52a596542b848f55fa372c75f14ac3695a9
Priya2123 <aggarwalpriya1121@gmail.com>
2023-10-16 17:32:02 +0530

    hacktoberfest

---
+++ src/components/pages/Demo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Demo = () => {
+  return (
+    <div>
+      <p>
+        The page you have requested does not exist. Please go to home page and
+        check again.
+      </p>
+    </div>
+  );
+};
+
+export default Demo;
